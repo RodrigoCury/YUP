cc14850d8163e17bea5d843660624b39bc4a32b8
Rodrigo Cury <rodrigocury@ufu.br>
2021-08-15 02:29:25 -0300

    Added Axios to implement db implementation

--- ypu/src/pages/Promotion/Search/Search.js
+++ ypu/src/pages/Promotion/Search/Search.js
@@ -1,30 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import PromotionCard from 'components/Promotion/Card/Card'
 
+import axios from "axios"
+
+
 const PagesPromotionSearch = () => {
-    const promotion = {
-        id: 1,
-        title: "Kit Notebook De fuder o Cu da Rabiola",
-        price: 1799.99,
-        imageUrl: "https://cdn.gatry.com/gatry-static/promocao/imagem/c3c533abbb0998197ece188605ad9caa.png",
-        url: "https://www.magazineluiza.com.br/notebook-lenovo-ideapad3i-82bs0005br-intel-core-i5-8gb-256gb-ssd-156-lcd-windows-10/p/135302600/in/note/",
-        comments: [
-            {
-                id: 1,
-                comment: "Eita Coroio"
-            }
-        ]
-    }
+
+    const [promotions, setPromotions] = useState([])
+
+    useEffect(() => {
+        axios.get('http://localhost:3340/promotions?_embed=comments')
+            .then(response => {
+                setPromotions(response.data)
+            })
+
+    }, [])
+
+
     return (
         <div
             style={{
                 maxWidth: 800,
                 margin: "30px auto"
             }}>
-            <PromotionCard
-                promotion={promotion}
-            />
+            {promotions.map(p => <PromotionCard promotion={p} />)}
+
         </div>
     );
 }
