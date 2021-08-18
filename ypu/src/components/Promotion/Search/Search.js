5331e28a3e5460150842b555b003b43a1c16f371
Rodrigo Cury <rodrigocury@ufu.br>
2021-08-18 15:45:40 -0300

    Using Custom API hook for Fetching Data

--- ypu/src/components/Promotion/Search/Search.js
+++ ypu/src/components/Promotion/Search/Search.js
@@ -1,36 +1,30 @@
 import React, { useEffect, useState } from 'react'
-import axios from 'axios'
 import { Link } from 'react-router-dom'
+import useApi from 'components/utils/useApi'
 import './Search.css'
 
 import PromotionList from '../List/List'
 
 const PromotionSearch = () => {
-    const [promotions, setPromotions] = useState([])
     const [search, setSearch] = useState('')
+    const [load, loadInfo] = useApi({
+        url: '/promotions',
+        method: 'get',
+        params: {
+            _embed: "comments",
+            _order: "desc",
+            _sort: "id",
+            title_like: search || undefined,
+        },
+    })
 
-    useEffect(() => {
-        const params = {}
-
-        if (search) {
-            params.title_like = search
-        }
-
-        setPromotions([])
-
-        axios.get('http://localhost:3340/promotions?_embed=comments&_order=desc&_sort=id', { params })
-            .then(response => {
-                setPromotions(response.data)
-            })
-
-    }, [search])
-
+    useEffect(load, [search])
 
     return (
         <div className='promotion-search'>
             <header className='promotion-search__header'>
                 <h1>Promo Show</h1>
-                <Link to="/create">Nova Promoção</Link>
+                <Link className='promotion-card__link' to="/create">Nova Promoção</Link>
             </header>
             <input
                 className='promotion-search__input'
@@ -39,7 +33,11 @@ const PromotionSearch = () => {
                 value={search}
                 onChange={(event) => setSearch(event.target.value)}
             />
-            <PromotionList promotions={promotions} loading={!promotions.length} />
+            <PromotionList
+                promotions={loadInfo.data}
+                loading={!loadInfo.loading}
+                error={loadInfo.error}
+            />
         </div>
     );
 
